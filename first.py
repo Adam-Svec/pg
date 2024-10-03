4234a0e2174cbf971c270dc3fe8e5c232adcf09a
Adam-Svec <svecad02@jcu.cz>
2024-10-03 09:07:45 +0000

    doplnění a úkol

--- first.py
+++ first.py
@@ -7,11 +7,15 @@ def sudy_lichy(cislo):
 sudy_lichy(5)
 sudy_lichy(1000000)
 #
-
+#úkol 1
 def sudy_lichy(cislo):
 
-    if cislo:
-    print ("sude")
+    if cislo % 2 == 0:
+        print("sude")
     
-    else:
-    print("liche")
+    else: 
+        print("liche")
+
+sudy_lichy(3)
+
+sudy_lichy(4)
