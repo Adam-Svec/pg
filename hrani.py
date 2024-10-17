437c68bc7a6a14c7b9e60534fc409ab1c0800db0
Adam-Svec <svecad02@jcu.cz>
2024-10-17 09:10:50 +0000

    cviceni 3

---
+++ hrani.py
@@ -0,0 +1,25 @@
+#def my_range(start,stop,step):
+#   vysledek = []
+#   while start < stop:
+#       vysledek.append(start)
+#        start +- step
+#    return vysledek
+
+def abceda(iterable,start=0):
+    result = []
+    index = start
+    for value in iterable
+        result.append((index , value))
+        index += 1
+    return result
+
+def abceda(iterable,start=0):
+    result = []
+    i=0
+    while i < len(iterable):
+        result.append((i, iterable[i]))
+
+def zip(*iterable):
+    interable = [list(it) for ]
+
+    return(print(list(zip([1,2,3],[4,5,6],[7,8,9],[10,11,12], ["a", "b", "c"]))))
