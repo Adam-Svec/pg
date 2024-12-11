bc59b987d681b7855bbca82ca7d1f5a15f40ee8d
Adam-Svec <svecad02@jcu.cz>
2024-12-11 16:37:59 +0000

    úkol 9

--- ninth.py
+++ ninth.py
@@ -1,19 +1,18 @@
 def dec_to_bin(cislo):
-    # Převedení vstupního čísla na integer, pokud je to řetězec
+  
     cislo = int(cislo)
     
-    # Speciální případ pro 0
     if cislo == 0:
         return "0"
     
-    # Inicializace prázdného řetězce pro binární reprezentaci
+    
     binary = ""
     
-    # Iterativní převod čísla na binární reprezentaci
+    #
     while cislo > 0:
-        # Přidání zbytku po dělení 2 na začátek řetězce
+        
         binary = str(cislo % 2) + binary
-        # Celočíselné dělení 2
+        
         cislo //= 2
     
     return binary
@@ -25,4 +24,4 @@ def test_bin_to_dec():
     assert dec_to_bin(101) == "1100101"
     assert dec_to_bin(127) == "1111111"
     assert dec_to_bin("128") == "10000000"
-    assert dec_to_bin(167) == "10100111"  # Přidán test pro 167
+    assert dec_to_bin(167) == "10100111"  
